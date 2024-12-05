381471bce42ad5276e6b0fcbb6554a8080595582
nwbengineer <nwbengineer@gmail.com>
2024-12-05 14:55:53 +0800

    Create client

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
@@ -10,6 +13,16 @@ type cliCommand struct {
 	name string
 	description string
 	callback func() error
+}
+
+type config struct {
+	Count    int    `json:"count,omitempty"`
+	Next     string `json:"next,omitempty"`
+	Previous any    `json:"previous,omitempty"`
+	Results  []struct {
+		Name string `json:"name,omitempty"`
+		URL  string `json:"url,omitempty"`
+	} `json:"results,omitempty"`
 }
 
 func getCommands() map[string]cliCommand {
@@ -24,6 +37,11 @@ func getCommands() map[string]cliCommand {
 			description: "Exit the Pokedex",
 			callback: commandExit,
 		},
+		"map": {
+			name: "map",
+			description: "Displays the Pokemon world map",
+			callback: commandMap,
+		},
 	}
 }
 
@@ -33,13 +51,19 @@ func main() {
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
 		input := scanner.Text()
-		switch input {
-		case "help":
-			commandHelp()
-		case "exit":
-			commandExit()
-		default:
-			fmt.Println("Unknown command:", input)
+
+		commands := getCommands()
+		if input == "map" {
+			url := config.Next
+			commands["map"].callback(url, &config)
+		}
+		if command, exists := commands[input]; exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+		} else {
+			fmt.Println("Unknown command. Type 'help' for a list of commands.")
 		}
 		}
 }
@@ -56,4 +80,38 @@ func commandHelp() error {
 func commandExit() error {
 	os.Exit(0)
 	return nil
+}
+
+func commandMap(url string, config *config) error {
+	if url == "" {
+		url = "https://pokeapi.co/api/v2/location-area/"
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", res.StatusCode)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, &config)
+	if err != nil {
+		return err
+	}
+
+	locationResults := config.Results
+	for _, locationResult := range locationResults {
+		fmt.Println(locationResult.Name)
+	}
+
+	return nil
+
 }
