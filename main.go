6e549c7f377d64cedb45275a283b03bde9f3d927
nwbengineer <nwbengineer@gmail.com>
2024-12-09 18:59:10 +0800

    refactor code structure. Add config per locations list request

--- main.go
+++ main.go
@@ -1,117 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
+	"time"
+
+	"github.com/Yrll21/pokedexcli/internal/pokeapi"
 )
 
-type cliCommand struct {
-	name string
-	description string
-	callback func() error
-}
-
-type config struct {
-	Count    int    `json:"count,omitempty"`
-	Next     string `json:"next,omitempty"`
-	Previous any    `json:"previous,omitempty"`
-	Results  []struct {
-		Name string `json:"name,omitempty"`
-		URL  string `json:"url,omitempty"`
-	} `json:"results,omitempty"`
-}
-
-func getCommands() map[string]cliCommand {
-	return map[string]cliCommand{
-		"help": {
-			name: "help",
-			description: "Displays a help message",
-			callback: commandHelp,
-		},
-		"exit": {
-			name: "exit",
-			description: "Exit the Pokedex",
-			callback: commandExit,
-		},
-		"map": {
-			name: "map",
-			description: "Displays the Pokemon world map",
-			callback: commandMap,
-		},
-	}
-}
-
 func main() {
-	for {
-		fmt.Print("pokedex > ")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		input := scanner.Text()
-
-		commands := getCommands()
-		if input == "map" {
-			url := config.Next
-			commands["map"].callback(url, &config)
-		}
-		if command, exists := commands[input]; exists {
-			err := command.callback()
-			if err != nil {
-				fmt.Println("Error:", err)
-			}
-		} else {
-			fmt.Println("Unknown command. Type 'help' for a list of commands.")
-		}
-		}
-}
-
-func commandHelp() error {
-	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
-	
-	for _, command := range getCommands() {
-		fmt.Printf("%s: %s\n", command.name, command.description)
-	}
-	return nil
-}
-
-func commandExit() error {
-	os.Exit(0)
-	return nil
-}
-
-func commandMap(url string, config *config) error {
-	if url == "" {
-		url = "https://pokeapi.co/api/v2/location-area/"
-	}
-	res, err := http.Get(url)
-	if err != nil {
-		return err
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
 	}
 
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %d", res.StatusCode)
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(body, &config)
-	if err != nil {
-		return err
-	}
-
-	locationResults := config.Results
-	for _, locationResult := range locationResults {
-		fmt.Println(locationResult.Name)
-	}
-
-	return nil
-
+	startRepl(cfg)
 }
